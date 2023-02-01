c8142fa98cd225061668ac7f55f9d523cd26936b
Max Ott <max.ott@data61.csiro.au>
2023-02-01 15:51:35 +1100

    cleaned up typing and moved example 'saver' function around

--- img_test_service.py
+++ img_test_service.py
@@ -1,18 +1,8 @@
 from PIL import Image, ImageDraw, ImageFont
-
-<<<<<<< HEAD
-from ivcap_sdk_service import Service, Parameter, Option, Type, IOAdapter, IOWritable, SupportedMimeTypes
-from ivcap_sdk_service import register_service, deliver_data, fetch_data, register_saver
-=======
-from ivcap_sdk_service import Service, Parameter, Type, SupportedMimeTypes, ServiceArgs
-from ivcap_sdk_service import register_service, deliver_data, fetch_data
->>>>>>> 985051b9a823491474f90ded5a91b277d996e840
 import logging
 
-from typing import Any, Dict
-
-import os
-import json
+from ivcap_sdk_service import Service, Parameter, Type, SupportedMimeTypes, ServiceArgs
+from ivcap_sdk_service import register_service, deliver_data, fetch_data, create_metadata
 
 logger = None # set when called by SDK
 
@@ -47,8 +37,6 @@ SERVICE = Service(
     ]
 )
 
-PNG_MT = 'image/png'
-
 def service(args: ServiceArgs, svc_logger: logging):
     global logger 
     logger = svc_logger
@@ -69,18 +57,21 @@ def service(args: ServiceArgs, svc_logger: logging):
     center = (args.width / 2, args.height / 2)
     canvas.text(center, args.msg, font=font, anchor='mm', fill=(255, 130, 0))   
     
-
-    # def png_saver(name: str, img: Any, io_adapter: IOAdapter, **kwargs):
-    #     print("IMG", str(type(img)))
-    #     fhdl: IOWritable = io_adapter.write_artifact(PNG_MT, f"{name}.png", **kwargs)
-    #     img.save(fhdl, format="png")
-    #     fhdl.close()
-    # register_saver(PNG_MT, None, png_saver)
-
-    meta = {
-       '@type': 'urn:ivcap.test:simple-python-service',
-        **args._asdict(),
-    }
+    meta = create_metadata('urn:ivcap.test:simple-python-service', **args._asdict())
     deliver_data("image.png", lambda fd: img.save(fd, format="png"), SupportedMimeTypes.JPEG, metadata=meta)
-    
+
+#####
+# An example of how to register an artifact saver for a non-supported Mime-Type
+# from typing import Any
+# from ivcap_sdk_service import IOAdapter, IOWritable, register_saver
+
+# PNG_MT = 'image/png'
+# def png_saver(name: str, img: Any, io_adapter: IOAdapter, **kwargs):
+#     fhdl: IOWritable = io_adapter.write_artifact(PNG_MT, f"{name}.png", **kwargs)
+#     img.save(fhdl, format="png")
+#     fhdl.close()
+# register_saver(PNG_MT, None, png_saver)
+
+####
+# Entry point
 register_service(SERVICE, service)
